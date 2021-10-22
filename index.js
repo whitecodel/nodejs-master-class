1e8ed7d2874d939f5c0ddac753062d7a1e43033a
Bhawani Shankar <henryroy40@gmail.com>
2021-10-22 18:56:47 +0530

    .

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+// for parsing
+app.use(express.json());
+
+// controllers
+function fn_get(req, res) {
+    console.log(req.query.name);
+    res.send("Hello World From GET");
+}
+
+app.get("/", function () {
+    trturnfn_get(req, res);
+});
+
+app.post("/", function (req, res) {
+    console.log(req.body.name);
+    return res.send("Hello World From POST");
+});
+
+app.put("/:id", function (req, res) {
+    console.log(req.params.id);
+    return res.send("Hello World From PUT");
+});
+
+app.delete("/:id", function (req, res) {
+    console.log(req.params.id);
+    return res.send("Hello World From DELETE");
+});
+
+app.listen(3000, function () {
+    console.log("app running on port 3000");
+});
